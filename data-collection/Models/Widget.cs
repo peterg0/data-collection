2379867704141ff3f11202e686bf0123611f9f2c
peterg0 <75237739+peterg0@users.noreply.github.com>
2023-08-17 08:32:44 -0500

    Add project files.

---
+++ data-collection/Models/Widget.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace data_collection.Models
+{
+    public class Widget
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime CreationDate { get; set; }
+        public string Type { get; set; }
+        public string SubType { get; set; }
+    }
+}
